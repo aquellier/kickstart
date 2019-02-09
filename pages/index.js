cda99594efda84e8c4cc2849f2579595c7275e11
antoinequellier <antoinecquellier@gmail.com>
2019-02-09 23:42:47 +0000

    add button to create a new campaign

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 
   // Next wants to be able to get the initial data without
@@ -25,6 +25,11 @@ class CampaignIndex extends Component {
     return <div>
       <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"/>
       {this.renderCampaigns()}
+      <Button
+        content="Create campaign"
+        icon="add circle"
+        primary
+      />
     </div>
   }
 }
