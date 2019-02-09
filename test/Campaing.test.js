f021638b29261fb115d79f2b5ff6bd2306f18d86
antoinequellier <antoinecquellier@gmail.com>
2019-02-09 13:41:06 +0000

    create construct folder for compilation + set up test file

---
+++ test/Campaing.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const compiledFactory = require('../ethereum/construct/CampaingFactory.json');
+const compiledCampaign = require('../ethereum/construct/Campaing.json');
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface));
+})
