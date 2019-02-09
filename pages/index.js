5e616dc80dbd436d7cf3cce742f1fe8ae6cbe252
antoinequellier <antoinecquellier@gmail.com>
2019-02-09 23:37:55 +0000

    add css to campaign lists

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,10 @@ class CampaignIndex extends Component {
     return <Card.Group items={items}/>
   }
   render () {
-
-    return <div>{this.renderCampaigns()}</div>
+    return <div>
+      <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"/>
+      {this.renderCampaigns()}
+    </div>
   }
 }
 
