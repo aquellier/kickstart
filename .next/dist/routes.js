98160f1383c7fe9bcc2ebc6d21e3375fd5e9e9ed
antoinequellier <antoinecquellier@gmail.com>
2019-02-13 23:19:05 +0000

    update routes for new request

--- .next/dist/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var routes = require('next-routes')();
-
-routes.add('/campaigns/new', '/campaigns/new').add('/campaigns/:address', '/campaigns/show').add('/campaigns/:address/requests', '/campaigns/requests/index');
-
-module.exports = routes;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwiYWRkIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZjs7QUFFQSxPQUNHLEFBREgsSUFDTyxBQURQLGtCQUN5QixBQUR6QixrQkFFRyxBQUZILElBRU8sQUFGUCx1QkFFOEIsQUFGOUIsbUJBR0csQUFISCxJQUdPLEFBSFAsZ0NBR3VDLEFBSHZDOztBQUtBLE9BQU8sQUFBUCxVQUFpQixBQUFqQiIsImZpbGUiOiJyb3V0ZXMuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL2FudG9pbmVxdWVsbGllci9jb2RlL2FxdWVsbGllci9zb2xpZGl0eS9raWNrc3RhcnQifQ==
--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
-
+import Layout from '../../../components/Layout';
+import { Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
 class RequestIndex extends Component {
+  static async getInitialProps(props) {
+    const { address } = pros.query;
+
+    return { address };
+  }
   render() {
     return (
-      <h3>Helloooo</h3>
+      <Layout>
+        <h3>Requsts</h3>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <Button primary>
+          <a>
+            New Request
+          </a>
+          </Button>
+        </Link>
+      </Layout>
     )
   }
 }
