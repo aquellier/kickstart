9ea54db31a6af81cb3fa85285f8e36f183a5ee3e
antoinequellier <antoinecquellier@gmail.com>
2019-02-13 23:30:43 +0000

    add inputs in form for new requests

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import { Form, Button, Message } from 'react';
+import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
+import { Link, Router } from 'routes';
+import { Input } from 'semantic-ui-react';
 
 class RequestNew extends Component {
+  state = {
+    value: '',
+    description: '',
+    recipient: ''
+  }
+  static async getInitialProps(props) {
+    const address = props.query;
+
+    return { address };
+  }
   render() {
     return(
-      <h3>Create a Request</h3>;
+      <Form>
+        <Form.Field>
+          <label>Description</label>
+          <Input
+            value={this.state.description}
+            onChange={event =>
+              this.setState({ description: event.target.value })}
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Value in Ether</label>
+          <Input
+            value={this.state.value}
+            onChange={event =>
+              this.setState({ value: event.target.value })}
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Recipient</label>
+          <Input
+            value={this.state.recipient}
+            onChange={event =>
+              this.setState({ recipient: event.target.value })}
+          />
+        </Form.Field>
+      </Form>
     )
   }
 }
